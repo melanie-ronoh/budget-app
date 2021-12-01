d3de95cbc75894eda8ae0702a1f59576cc5b8a5e
Melanie Ronoh <melanie.ronoh@gmail.com>
2021-12-01 15:28:26 -0600

    budget-app

---
+++ src/context/AppContext.js
@@ -0,0 +1,56 @@
+import React, { createContext, useReducer } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+export const AppReducer = (state, action) => {
+	switch (action.type) {
+		case 'ADD_EXPENSE':
+			return {
+				...state,
+				expenses: [...state.expenses, action.payload],
+			};
+		case 'DELETE_EXPENSE':
+			return {
+				...state,
+				expenses: state.expenses.filter(
+					(expense) => expense.id !== action.payload
+				),
+			};
+		case 'SET_BUDGET':
+			return {
+				...state,
+				budget: action.payload,
+			};
+
+		default:
+			return state;
+	}
+};
+
+const initialState = {
+	budget: 5000,
+	expenses: [
+		{ id: uuidv4(), name: 'Credit Card', cost: 30 },
+		{ id: uuidv4(), name: 'Vacation', cost: 100 },
+		{ id: uuidv4(), name: 'Oil Change', cost: 70 },
+		{ id: uuidv4(), name: 'Apt', cost: 40 },
+	],
+};
+
+export const AppContext = createContext();
+
+
+export const AppProvider = (props) => {
+	const [state, dispatch] = useReducer(AppReducer, initialState);
+
+	return (
+		<AppContext.Provider
+			value={{
+				expenses: state.expenses,
+				budget: state.budget,
+				dispatch,
+			}}
+		>
+			{props.children}
+		</AppContext.Provider>
+	);
+};
